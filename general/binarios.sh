7e852d18c8dab06309799a206747715c68768049
PROTOKATANA <PROTOKATANA@proton.me>
2026-03-05 00:33:49 +0100

    +-+-+-+-+

---
+++ general/binarios.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/bash
+
+if [ "$EUID" -ne 0 ] ; then echo "EJECUTAR SCRIPT COMO USUARIO ROOT" ; exit 1 ; fi
+
+binarios=(
+
+	nano
+
+	vim
+
+	netpbm
+
+	samba-client
+
+	cifs-utils
+
+	qt6-base-common-devel
+
+	qt6-declarative-tools
+
+	groff-full
+
+	ghostscript
+
+	libpaper-tools
+
+	screen
+
+	sg3_utils
+
+	smp_utils
+
+	sharutils
+
+	wol
+
+	procmail
+
+	openslp
+
+	ruby4.0
+
+	acpica
+
+	net-snmp
+
+	augeas
+
+)
+
+delete() {
+
+	local exe="$1"
+
+	if [ -z "$exe" ]; then return 1 ; fi
+
+	if rpm -q "$exe" &>/dev/null ; then zypper remove --clean-deps -y "$exe" ;
+
+    else echo "EXTINTO : [$exe]" ; fi
+
+}
+
+for iter in "${binarios[@]}" ; do delete "$iter" ; done
